27e7de26614e81e71245ab95668b251f8f482311
Johnson <chnejohnson@gmail.com>
2023-06-06 23:22:10 +0800

    dev: capture user event

--- src/contentScripts/views/App.vue
+++ src/contentScripts/views/App.vue
@@ -3,21 +3,44 @@ import { useToggle } from '@vueuse/core'
 import 'uno.css'
 
 const [show, toggle] = useToggle(false)
+
+const content = ref('')
+
+onMounted(() => {
+  // document.addEventListener('focus', handleFocusEvent, true)
+  // document.addEventListener('blur', handleBlurEvent, true)
+  document.addEventListener('input', handleInputEvent, true)
+  document.addEventListener('mouseup', handleMouseUpEvent)
+})
+
+function handleInputEvent(event: Event) {
+  const target = event.target
+  if (target instanceof HTMLInputElement || target instanceof HTMLTextAreaElement)
+    content.value = target.value
+}
+
+function handleMouseUpEvent() {
+  if (window) {
+    const selectedText = window.getSelection()?.toString()
+    if (selectedText)
+      content.value = selectedText
+  }
+}
 </script>
 
 <template>
-  <div class="fixed right-0 bottom-0 m-5 z-100 flex items-end font-sans select-none leading-1em">
+  <div class="fixed right-0 bottom-0 m-5 z-100 flex flex-col items-end font-sans select-none leading-1em">
     <div
-      class="bg-white text-gray-800 rounded-lg shadow w-max h-min"
+      class="bg-white text-gray-800 rounded-lg shadow h-[500px] w-[200px]"
       p="x-4 y-2"
-      m="y-auto r-2"
+      m="y-auto b-2"
       transition="opacity duration-300"
       :class="show ? 'opacity-100' : 'opacity-0'"
     >
       <h1 class="text-lg">
-        Vitesse WebExt
+        Translator
       </h1>
-      <SharedSubtitle />
+      <p>{{ content }}</p>
     </div>
     <button
       class="flex w-10 h-10 rounded-full shadow cursor-pointer border-none"
